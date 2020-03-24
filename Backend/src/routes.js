29ac8087c467c821034f254fba136e2ad1055f1c
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2020-03-23 22:09:26 -0300

    loader model user

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Paulo Ricardo Jr',
+        email: 'pauloricardo@fastfeet.com.br',
+        password_hash: '123456',
+    });
+    res.json(user);
+});
 
 export default routes;
