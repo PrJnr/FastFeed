f3520e3699fcfb3fec0d90ef1fcd029ff610316f
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2020-03-27 21:55:59 -0300

    adding validation data entry

--- Backend/src/app/controllers/RecipientsController.js
+++ Backend/src/app/controllers/RecipientsController.js
@@ -3,6 +3,12 @@ import Recipients from '../models/Recipients';
 class RecipientsController {
     // eslint-disable-next-line class-methods-use-this
     async store(req, res) {
+        const recipientsExists = await Recipients.findOne({
+            where: { name: req.body.name },
+        });
+        if (recipientsExists) {
+            return res.status(400).json({ erro: 'Recipients already exists' });
+        }
         const recipients = await Recipients.create(req.body);
 
         return res.json(recipients);
