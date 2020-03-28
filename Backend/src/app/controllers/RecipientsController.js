1d4c4a3f846cb836507a36bc7c79b75111b38dd4
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2020-03-27 21:38:13 -0300

    updated recipients controller and adding route

--- Backend/src/app/controllers/RecipientsController.js
+++ Backend/src/app/controllers/RecipientsController.js
@@ -1,3 +1,12 @@
-class RecipientsController {}
+import Recipients from '../models/Recipients';
+
+class RecipientsController {
+    // eslint-disable-next-line class-methods-use-this
+    async store(req, res) {
+        const recipients = await Recipients.create(req.body);
+
+        return res.json(recipients);
+    }
+}
 
 export default new RecipientsController();
--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
+import RecipientsController from './app/controllers/RecipientsController';
 
 const routes = new Router();
 
-routes.get();
+routes.post('/recipients', RecipientsController.store);
 
 routes.post('/sessions', SessionController.store);
 
